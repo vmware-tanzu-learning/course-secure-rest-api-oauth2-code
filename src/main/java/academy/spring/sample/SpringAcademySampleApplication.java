6a2d57a296f35393d5537566bae8f8481c809055
Joe Moore <joe.moore@gmail.com>
2023-03-15 13:49:57 -0600

    <initializr-end> <first-lab-start> Codebase from Spring Initializr

---
+++ src/main/java/academy/spring/sample/SpringAcademySampleApplication.java
@@ -0,0 +1,13 @@
+package academy.spring.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAcademySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAcademySampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/spring/sample/SpringAcademySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.spring.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAcademySampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
