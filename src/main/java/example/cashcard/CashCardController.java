c7d4b37ed977605357f58a0682bc3fdcff36dd2e
Joe Moore <joe.moore@gmail.com>
2023-03-15 13:49:57 -0600

    Updated to mockmvc tests

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,49 @@
+package example.cashcard;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cashcards")
+public class CashCardController {
+    private CashCardRepository cashCardRepository;
+
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
+    @GetMapping("/{requestedId}")
+    public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<CashCard>> findAll() {
+        return ResponseEntity.ok(cashCardRepository.findAll());
+    }
+}
---
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,64 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CashCardApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnACashCardWhenDataIsSaved() throws Exception {
+        mockMvc.perform(get("/cashcards/99"))
+                .andExpect(jsonPath("$.id").value(99))
+                .andExpect(jsonPath("$.amount").value(123.45));
+    }
+
+    @Test
+    void shouldNotReturnACashCardWithAnUnknownId() throws Exception {
+        mockMvc.perform(get("/cashcards/1000"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(""));
+    }
+
+    @Test
+    @DirtiesContext
+    void shouldCreateANewCashCard() throws Exception {
+        String location = mockMvc.perform(post("/cashcards")
+                .contentType("application/json")
+                .content("""
+                        {
+                            "amount" : 250.00,
+                            "owner" : "sarah1"
+                        }
+                       """))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andReturn().getResponse().getHeader("Location");
+
+        mockMvc.perform(get(location))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.amount").value(250.00));
+    }
+
+    @Test
+    void shouldReturnAllCashCardsWhenListIsRequested() throws Exception {
+         mockMvc.perform(get("/cashcards"))
+                 .andExpect(jsonPath("$.length()").value(3))
+                 .andExpect(jsonPath("$..id").value(containsInAnyOrder(99, 100, 101)))
+                 .andExpect(jsonPath("$..amount").value(containsInAnyOrder(123.45, 1.00, 150.00)));
+    }
+}
