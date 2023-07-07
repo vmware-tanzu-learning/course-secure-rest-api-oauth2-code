ddd5d5b24a90a4b993227c94341991ff27910014
Joe Moore <joe.moore@gmail.com>
2023-07-07 13:49:48 -0600

    <spring-security-defaults-end> <bearer-jwt-start>

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -4,16 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser
 class CashCardApplicationTests {
 
     @Autowired
@@ -23,13 +26,14 @@ class CashCardApplicationTests {
     @DirtiesContext
     void shouldCreateANewCashCard() throws Exception {
         mockMvc.perform(post("/cashcards")
-                .contentType("application/json")
-                .content("""
-                        {
-                            "amount" : 250.00,
-                            "owner" : "sarah1"
-                        }
-                       """))
+                        .with(csrf())
+                        .contentType("application/json")
+                        .content("""
+                                 {
+                                     "amount" : 250.00,
+                                     "owner" : "sarah1"
+                                 }
+                                """))
                 .andExpect(status().isCreated())
                 .andExpect(header().exists("Location"))
                 .andReturn().getResponse().getHeader("Location");
@@ -37,9 +41,9 @@ class CashCardApplicationTests {
 
     @Test
     void shouldReturnAllCashCardsWhenListIsRequested() throws Exception {
-         mockMvc.perform(get("/cashcards"))
-                 .andExpect(jsonPath("$.length()").value(3))
-                 .andExpect(jsonPath("$..id").value(containsInAnyOrder(99, 100, 101)))
-                 .andExpect(jsonPath("$..amount").value(containsInAnyOrder(123.45, 1.00, 150.00)));
+        mockMvc.perform(get("/cashcards"))
+                .andExpect(jsonPath("$.length()").value(3))
+                .andExpect(jsonPath("$..id").value(containsInAnyOrder(99, 100, 101)))
+                .andExpect(jsonPath("$..amount").value(containsInAnyOrder(123.45, 1.00, 150.00)));
     }
 }
