f320710d7d61173763455a5bb3e65a2a733b20b9
Felipe <felipeg@gmail.com>
2023-07-07 10:51:06 -0400

    Solution #3 - Module 2 - Step 6 <m2-s6-accessing-auth-end> <m2-s9-auth-anywhere-start>
    Please see the following as a step-by-step reference for the
    changes to findAll:
    
        // (1)
        /*
        @GetMapping
        public ResponseEntity<Iterable<CashCard>> findAll(Authentication authentication) {
            var result = new ArrayList<CashCard>();
    
            cashCardRepository.findAll().forEach(cashCard -> {
                if (cashCard.owner().equals(authentication.getName())){
                    result.add(cashCard);
                }
            });
    
            return ResponseEntity.ok(result);
        }
        */
    
        // (2)
        /*
        @GetMapping
        public ResponseEntity<Iterable<CashCard>> findAll(Authentication authentication) {
            var result = cashCardRepository.findByOwner(authentication.getName());
            return ResponseEntity.ok(result);
        }
        */
    
        // (3)
        /*
        @GetMapping
        public ResponseEntity<Iterable<CashCard>> findAll(@CurrentSecurityContext SecurityContext securityContext) {
            var result = cashCardRepository.findByOwner(securityContext.getAuthentication().getName());
            return ResponseEntity.ok(result);
        }
        */
    
        // (4)
        /*
        @GetMapping
        public ResponseEntity<Iterable<CashCard>> findAll(@CurrentSecurityContext(expression="authentication.name") String owner) {
            var result = cashCardRepository.findByOwner(owner);
            return ResponseEntity.ok(result);
        }
        */
    
        // (5)
        /*
        @GetMapping
        public ResponseEntity<Iterable<CashCard>> findAll(@CurrentOwner String owner) {
            var result = cashCardRepository.findByOwner(owner);
            return ResponseEntity.ok(result);
        }
        */

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -1,7 +1,6 @@
 package example.cashcard;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,17 +8,15 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@WithMockUser
+@WithMockUser(username = "sarah1")
 class CashCardApplicationTests {
 
     @Autowired
@@ -33,6 +30,7 @@ class CashCardApplicationTests {
                 .andExpect(jsonPath("$.owner").value("sarah1"));
     }
 
+    @WithMockUser(username="esuez5")
     @Test
     @DirtiesContext
     void shouldCreateANewCashCard() throws Exception {
@@ -41,8 +39,7 @@ class CashCardApplicationTests {
                         .contentType("application/json")
                         .content("""
                         {
-                            "amount" : 250.00,
-                            "owner"  : "sarah1"
+                            "amount" : 250.00
                         }
                         """))
                 .andExpect(status().isCreated())
@@ -52,15 +49,14 @@ class CashCardApplicationTests {
         this.mvc.perform(get(location))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.amount").value(250.00))
-                .andExpect(jsonPath("$.owner").value("sarah1"));
+                .andExpect(jsonPath("$.owner").value("esuez5"));
     }
 
     @Test
     void shouldReturnAllCashCardsWhenListIsRequested() throws Exception {
         this.mvc.perform(get("/cashcards"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()").value(3))
-                .andExpect(jsonPath("$..owner").value(hasItem("sarah1")))
-                .andExpect(jsonPath("$..owner").value(hasItem("esuez5")));
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$..owner").value(everyItem(equalTo("sarah1"))));
     }
 }
