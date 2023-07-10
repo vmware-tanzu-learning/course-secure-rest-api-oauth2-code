b9817a95326bdd5a777ff21cefb3b6993b3f36f8
Felipe <felipeg@gmail.com>
2023-07-10 09:32:00 -0400

    ready

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -3,6 +3,8 @@ package example.cashcard;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -116,11 +118,11 @@ public class CashCardController {
     */
 
     // (6)
-
+    /*
     @GetMapping
     public ResponseEntity<Iterable<CashCard>> findAll(@AuthenticationPrincipal Jwt jwt) {
         var result = cashCardRepository.findByOwner(jwt.getSubject());
         return ResponseEntity.ok(result);
     }
-
+    */
 }
