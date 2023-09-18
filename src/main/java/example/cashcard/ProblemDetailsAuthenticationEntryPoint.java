7c987ab80933ae0f9518f0b9ecce4965db477d8d
Josh Cummings <josh.cummings@gmail.com>
2023-09-18 17:25:10 -0600

    Solution #6 - Module 2 - Step 13 <m2-s13-processing-failures-end> <m3-s4-request-auth-start>

--- src/main/java/example/cashcard/ProblemDetailsAuthenticationEntryPoint.java
+++ src/main/java/example/cashcard/ProblemDetailsAuthenticationEntryPoint.java
@@ -17,9 +17,24 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ProblemDetailsAuthenticationEntryPoint implements AuthenticationEntryPoint {
+	private final AuthenticationEntryPoint delegate = new BearerTokenAuthenticationEntryPoint();
+
+	private final ObjectMapper mapper;
+
+	public ProblemDetailsAuthenticationEntryPoint(ObjectMapper mapper) {
+		this.mapper = mapper;
+	}
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
 			throws IOException, ServletException {
+		this.delegate.commence(request, response, authException);
+		if (authException.getCause() instanceof JwtValidationException validation) {
+			ProblemDetail detail = ProblemDetail.forStatus(401);
+			detail.setType(URI.create("https://tools.ietf.org/html/rfc6750#section-3.1"));
+			detail.setTitle("Invalid Token");
+			detail.setProperty("errors", validation.getErrors());
+			this.mapper.writeValue(response.getWriter(), detail);
+		}
 	}
 }
--- src/test/java/example/cashcard/CashCardSpringSecurityTests.java
+++ src/test/java/example/cashcard/CashCardSpringSecurityTests.java
@@ -24,9 +24,11 @@ import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -56,6 +58,20 @@ public class CashCardSpringSecurityTests {
 		this.mvc.perform(get("/cashcards/1000").header("Authorization", "Bearer " + token))
 				.andExpect(status().isUnauthorized())
 				.andExpect(header().string("WWW-Authenticate", containsString("Jwt expired")));
+	}
+
+	@Test
+	void shouldShowAllTokenValidationErrors() throws Exception {
+		String expired = mint((claims) -> claims
+				.audience(List.of("https://wrong"))
+				.issuedAt(Instant.now().minusSeconds(3600))
+				.expiresAt(Instant.now().minusSeconds(3599))
+		);
+		this.mvc.perform(get("/cashcards").header("Authorization", "Bearer " + expired))
+				.andExpect(status().isUnauthorized())
+				.andExpect(header().exists("WWW-Authenticate"))
+				.andExpect(jsonPath("$.errors..description").value(
+						containsInAnyOrder(containsString("Jwt expired"), containsString("aud claim is not valid"))));
 	}
 
 	private String mint() {
