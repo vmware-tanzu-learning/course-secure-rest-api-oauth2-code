545cdde2a7e4dc38ab5c78a7f199c9cd687d9bae
Josh Cummings <josh.cummings@gmail.com>
2023-09-18 17:25:10 -0600

    Solution #6 - Module 2 - Step 13 <m2-s13-processing-failures-end> <m3-s4-request-auth-start>

--- src/main/java/example/cashcard/CashCardApplication.java
+++ src/main/java/example/cashcard/CashCardApplication.java
@@ -2,6 +2,11 @@ package example.cashcard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * The application entry point
@@ -11,6 +16,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class CashCardApplication {
+
+	@Bean
+	SecurityFilterChain appSecurity(HttpSecurity http, AuthenticationEntryPoint entryPoint)
+			throws Exception {
+		http
+			.authorizeHttpRequests((authorize) -> authorize
+				.anyRequest().authenticated()
+			)
+			.oauth2ResourceServer((oauth2) -> oauth2
+				.authenticationEntryPoint(entryPoint)
+				.jwt(Customizer.withDefaults())
+			);
+		return http.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CashCardApplication.class, args);
